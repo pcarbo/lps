e3ef4fed034852be8f9be7e5a6d4168266bff570
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-05-02 16:08:58 -0500

    Added steps to fit Poisson NMF model in fit_topic_model.R.

--- scripts/fit_topic_model.R
+++ scripts/fit_topic_model.R
@@ -12,4 +12,13 @@ samples <- strsplit(samples,"_")
 samples <- data.frame(tissue    = sapply(samples,"[[",1),
                       timepoint = sapply(samples,"[[",2),
                       mouse     = sapply(samples,"[[",3))
-                     
+
+# Remove genes with very low (or no) expression.
+j <- which(colSums(counts) > 20)
+counts <- counts[,j]
+
+# Fit a Poisson NMF model with k = 13 topics.
+fit <- fit_poisson_nmf(counts,k = 13,init.method = "random",method = "em",
+                       numiter = 20,control = list(nc = 2))
+fit <- fit_poisson_nmf(counts,fit0 = fit,method = "scd",numiter = 180,
+                       control = list(numiter = 4,nc = 2,extrapolate = TRUE))
